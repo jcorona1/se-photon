0313083a43b3c831953dcfa58256b0c68e5d1ee1
Hayden <76058125+HaM2703@users.noreply.github.com>
2025-04-10 10:11:19 -0500

    Create Game.bash

---
+++ Game.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Launch Server.bash in a new terminal window
+xterm -geometry 120x24+1500+800 -hold -e "./Server.bash" &
+sleep 1
+
+# Launch traffic.py in another new terminal window
+xterm -geometry 120x24+1500+0 -hold -e "python3 python_trafficgenarator_v2.py" &
+sleep 4
+
+set -e
+javac *.java
+java Game
